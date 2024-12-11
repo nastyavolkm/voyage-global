f8a0fe75ec40152ccd5846f1547016cf8573627b
Anastasiya_Mianko <anastasiya_mianko@epam.com>
2024-12-11 15:10:13 +0300

    add new bg

--- my-app/src/pages/TourSearchPage.tsx
+++ my-app/src/pages/TourSearchPage.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import stub from '../assets/tour-search.png';
 import './TourSearchPage.scss';
 
 export function TourSearchPage() {
     return (
-        <div className='search'>
-            <img
-                className='search__img'
-                src={stub}
-                alt='search'
-            />
-        </div>
+        <></>
     )
 }
